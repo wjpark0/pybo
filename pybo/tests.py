554f9ff1f57035942cb9241d0222c2a0729eb909
wjpark <wjpark@ecosian.com>
2024-09-20 09:13:11 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from pybo.models import Question
+from django.utils import timezone
+
+# Create your tests here.
+for i in range(300):
+    q = Question(subject='테스트 데이터입니다:[%03d]' % i, content='내용무', create_date=timezone.now())
+    q.save()
